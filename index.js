09fdd3e407e6098bc27a7b0a52553de933766a5f
Ivan Demidov <demidov@220-volt.ru>
2016-03-21 16:22:37 +0300

    Resolve root path for windows

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 const path = require('path');
 
 exports = module.exports = function (options) {
-	var pkg = require(path.join(process.env.PWD, 'package.json'));
+	var pkg = require(path.join((process.env.PWD || path.dirname(require.main.filename)), 'package.json'));
 
 	if (typeof options === 'string') {
 		options = require(path.join(process.cwd(), options));
