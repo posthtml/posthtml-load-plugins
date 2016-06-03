e4b2005c2ddb6e68eddd3c1e21dba9a7f6cf934b
Ivan Demidov <demidov@220-volt.ru>
2016-06-03 13:02:09 +0300

    Fixed #20, not work if options for pligins is a string

--- test/test-plugin.js
+++ test/test-plugin.js
@@ -24,3 +24,10 @@ test('Plugin reads custom json config from posthtml.json', async t => {
 		(await posthtml(plugin('fixtures/posthtml.json')).process(await read('fixtures/input.html'))).html
 	);
 });
+
+test('test witch posthtml-css-modules', async t => {
+	t.is(
+		(await read('expected/output-modules.html')),
+		(await posthtml(plugin('fixtures/modules.json', {'posthtml-css-modules': 'dist/css-modules.json', 'posthtml-each': {}})).process(await read('fixtures/input-modules.html'))).html
+	);
+});
